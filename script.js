ee0890263708cee0fc40d6cf9abfa6832ceecbad
Vishakha Gautam <156678713+Vishakha2431@users.noreply.github.com>
2026-04-21 21:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let sum=0
+	
+	while(arr.length>1){
+		arr.sort((a,b)=>a-b)
+		let a=arr.shift()
+		let b=arr.sift()	
+		sum+=a+b
+		arr.push(sum)
+		
+		
+	}
+	return sum
   
 }
 
